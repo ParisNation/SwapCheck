19733cf1efd588888ae39cb9d9d91caa2315e093
Paris Humphrey <parishjr@gmail.com>
2026-05-12 16:13:25 -0400

    Add Swagger UI, seeder, working vehicles endpoint - SwapCheck v1

--- SwapCheck.API/Program.cs
+++ SwapCheck.API/Program.cs
@@ -11,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<SwapCheckDbContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
@@ -26,6 +29,8 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseSwagger();
+    app.UseSwaggerUI();
     app.MapOpenApi();
 }
 
